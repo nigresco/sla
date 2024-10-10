5d0b298b3664fa77072f08da797fecad9ce85e50
digresco <166826138+nigresco@users.noreply.github.com>
2024-10-10 12:26:38 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,16 @@
+ciarCartao(
+    'Programação',
+    'O Que é Python?',
+    'O Pythin é uma linguagem de programação'
+)
+ciarCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+ciarCartao(
+    'Programação',
+    'O Que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
